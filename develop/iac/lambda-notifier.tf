68ccd2ba286db7ff5cd76b796dc23ac18a2e0112
Francisco Valverde Vasquez <julval_09@hotmail.com>
2025-05-01 00:07:16 -0500

    feat: agrega recurso Lambda Bucket Notifcation y S3

--- develop/iac/lambda-notifier.tf
+++ develop/iac/lambda-notifier.tf
@@ -58,3 +58,22 @@ resource "aws_lambda_function" "notifier" {
     }
   }
 }
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notifier.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.notifier.arn
+    events             = ["s3:ObjectCreated:*"]
+  }
+
+    depends_on = [aws_lambda_permission.allow_s3]
+}

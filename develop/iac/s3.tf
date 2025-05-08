40da5379df47a1df9f5cc412f9f586201f7807d7
Medina-Tirado-Diego <dmedinat1@upao.edu.pe>
2025-05-08 01:30:20 -0500

    test 3

--- develop/iac/s3.tf
+++ develop/iac/s3.tf
@@ -1,30 +1,3 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "notifier-bucket-project-${var.environment}"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  logging {
-    target_bucket = aws_s3_bucket.logs.id
-    target_prefix = "s3-logs/"
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Environment = var.environment
-  }
-}
-
-resource "aws_s3_bucket" "logs" {
-  bucket = "online-ready-logs-${var.environment}"
-  acl    = "log-delivery-write"
+  bucket = "notifier-bucket-project"
 }
